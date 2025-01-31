b773cbd192171eb486975463c4cfd3728e61dcdd
Maksym Norokha <maxnorohich@gmail.com>
2025-01-31 22:43:32 +0100

    init admin and fair services

---
+++ backend/fair-service/src/test/java/com/hacknimeto/fair_service/FairServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacknimeto.fair_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
